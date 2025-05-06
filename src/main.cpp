47511e5261b76a40600646029a954daa8e35dd61
TimeStepYT <herwig.orlich@gmail.com>
2025-05-06 20:35:33 +0200

    I can't do this shit anymore, none of these touchdelegates work

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ class $modify(NekoMenuLayer, MenuLayer) {
 
 #include <Geode/modify/GJListLayer.hpp>
 class $modify(NekoListLayer, GJListLayer) {
-    bool init(BoomListView * p0, char const* p1, ccColor4B p2, float p3, float p4, int p5) {
+    bool init(BoomListView* p0, char const* p1, ccColor4B p2, float p3, float p4, int p5) {
         if (!GJListLayer::init(p0, p1, p2, p3, p4, p5)) return false;
 
         CreateNekoEvent("create-neko"_spr, this).post();
@@ -51,7 +51,7 @@ class $modify(NekoListLayer, GJListLayer) {
 #ifdef CHAOS_MODE
 #include <Geode/modify/CCMenuItemSpriteExtra.hpp>
 class $modify(NekoMenuItemSpriteExtra, CCMenuItemSpriteExtra) {
-    bool init(CCNode * sprite, CCNode * disabledSprite, CCObject * target, SEL_MenuHandler callback) {
+    bool init(CCNode* sprite, CCNode* disabledSprite, CCObject* target, SEL_MenuHandler callback) {
         if (!CCMenuItemSpriteExtra::init(sprite, disabledSprite, target, callback)) return false;
 
         CreateNekoEvent("create-neko"_spr, this).post();
